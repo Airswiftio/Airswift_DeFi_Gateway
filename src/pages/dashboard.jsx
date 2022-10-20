5bdb63765a6f7b9e62270aca7418e1760f431fd4
Nicolas Partridge <nicolas@nicolass-Air-3.hitronhub.home>
2022-10-20 10:17:43 -0700

    core(dashboard): Updated dashboard page with general layout + components

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,12 +1,29 @@
-import React from "react";
-
+import React, { useState } from "react";
+import InfoCard from "../components/cards/InfoCard";
 import { Navbar } from "../components/";
 
+import "./dashboard.scss";
+
 const Dashboard = () => {
+  const [timeframe, setTimeframe] = useState(0);
   return (
     <div>
       <Navbar />
-      Dashboard
+      <div className="dashboardContent">
+        <div className="cardRow">
+          <InfoCard title="Total Balance" value={9898} type={0} />
+          <InfoCard title="Total Balance" value={4556} type={0} />
+        </div>
+        <div className="balanceOverview">
+          <span>Balance Overview</span>
+          <div className="buttonSelectors">
+            <button onClick={() => setTimeframe(0)}>24H</button>
+            <button onClick={() => setTimeframe(1)}>7D</button>
+            <button onClick={() => setTimeframe(2)}>1M</button>
+          </div>
+          <div className="chart"></div>
+        </div>
+      </div>
     </div>
   );
 };
